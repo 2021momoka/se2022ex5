764f3cc22e99d9446b5fbf879c07d4043531bdc3
momoka <r202103310op.jindai.jp>
2023-01-28 23:37:27 +0900

    teisei-tuika

--- main.c
+++ main.c
@@ -1,6 +1,9 @@
 #include <stdio.h> 
 int main(void){
     int i = 10;
-     printf("Hello World %d\n"); 
+    int n = 100;
+    n = i;
+     printf("Hello World %d\n",i);
+     printf("n = %d\n",n) 
      return 0; 
 }

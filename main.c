d14cea53b38b53a0d7f1f1fe4ad8c8bed366c1f5
momoka <r202103310op.jindai.jp>
2023-01-28 23:40:29 +0900

    forloop

--- main.c
+++ main.c
@@ -1,9 +1,6 @@
 #include <stdio.h> 
 int main(void){
-    int i = 10;
-    int n = 100;
-    n = i;
-     printf("Hello World %d\n",i);
-     printf("n = %d\n",n) 
+    for(int i = 0;i<5;i++)
+     printf("i=%d\n",i);
      return 0; 
 }
